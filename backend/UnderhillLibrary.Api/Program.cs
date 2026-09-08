dad3ba148913806c6f04ccbd401b66958da8d66a
Anna <217314484+Demonetic@users.noreply.github.com>
2026-09-08 13:44:59 +0200

    chore: apply database migrations on startup

--- backend/UnderhillLibrary.Api/Program.cs
+++ backend/UnderhillLibrary.Api/Program.cs
@@ -56,6 +56,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+	var dbContext = scope.ServiceProvider
+		.GetRequiredService<UnderhillLibraryDbContext>();
+
+	await dbContext.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
